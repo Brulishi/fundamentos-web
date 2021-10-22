13fdc5b7c8821b8ed61ad1ec6989f63f5f66f1a1
Pablo <bruli_pablo@hotmail.com>
2021-10-22 17:41:29 -0300

    [Feature Pablo - Task: começando javascript

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Declaração de variaveis
+
+// var nome = 'Pablo' //modelo mais antigo e em desuso de variavel
+// let nome2 = "bruli" //variavel para quando irão existir mudanças
+// const nome3 = "pablito" //varivavel fixa
+// por Tag:getElementByTagName()
+// por Id: getElementById()
+// por Nome: getelementsByName()
+// por Classe : getElementsByClassName()
+// por Seletor: querySelector()
+
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+
+nome.style.width = "100%"
+email.style.width = '100%'
